9b19a85921b9d9a6ee0915f7b18063420e46d058
Phuong <vuminhphuongvt12345@gmail.com>
2021-11-01 12:15:41 +0700

    test

---
+++ 1.11/idk/a.cpp
@@ -0,0 +1,28 @@
+// Author : MinhPhuongVu A.K.A SunnyYeahBoii
+
+/* 
+Code Ideal: I'm too lazy for something like this .__.
+just testing
+*/
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define NAME "remizdabest"
+#define FileInput() if(NAME != "remizdabest"){freopen(NAME".inp" , "r" , stdin);freopen(NAME".out" , "w" , stdout);}
+#define int long long
+#define endl "\n"
+#define INF 1 << 30
+
+void solve(){
+		
+}
+
+int32_t main(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);cout.tie(0);
+        FileInput();
+        solve();
+        return 0;
+}        
