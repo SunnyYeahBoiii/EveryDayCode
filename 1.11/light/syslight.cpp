61836aa70073d157bdeada76db54439335bd5ea0
Phuong <vuminhphuongvt12345@gmail.com>
2021-11-01 11:20:38 +0700

    update syslight.cpp

--- 1.11/light/syslight.cpp
+++ 1.11/light/syslight.cpp
@@ -2,20 +2,34 @@
 
 /* 
 Code Ideal: I'm too lazy for something like this .__.
+
+Simple DP
+
+dp[i] = dp[i-2] + dp[i-1]
+
+I'm not really sure about this .__.
 */
 
 #include "bits/stdc++.h"
 
 using namespace std;
 
-#define NAME "remizdabest"
+#define NAME "syslight"
 #define FileInput() if(NAME != "remizdabest"){freopen(NAME".inp" , "r" , stdin);freopen(NAME".out" , "w" , stdout);}
 #define int long long
 #define endl "\n"
 #define INF 1 << 30
 
 void solve(){
-		
+	int n;
+	cin >> n;
+	int dp[n+2];
+	dp[1] = 1;
+	dp[2] = 1;
+	for(int i = 3 ; i <= n ; i++)
+		dp[i] = dp[i-2] + dp[i-1];
+
+	cout << dp[n] << endl;
 }
 
 int32_t main(){
