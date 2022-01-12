f5cbce07382b92c6d179b38be2a96fc7918eb4e0
SunnyYeahBoiii <vuminhphuongvt12345@gmail.com>
2022-01-12 20:46:25 +0700

    update

--- Code của Sunny/OnClass/10.10Tin_Thầy_Huy/redtowel/redtowel.cpp
+++ Code của Sunny/OnClass/10.10Tin_Thầy_Huy/redtowel/redtowel.cpp
@@ -14,15 +14,6 @@ using namespace std;
 #define endl "\n"
 #define INF 1 << 30
 
-int w , h;
-int a[105][105];
-bool safe(int x , int y ){
-	if(0 <= x && x <= w && 0 <= y && y <= h)
-		if(a[x][y] == 0)
-			return true;
-	return false;
-}
-
 int to_num(char a){
         return a-'0';
 }
@@ -56,69 +47,85 @@ string cong(string a, string b){
 }
 
 
-void semt(){
-	for(int i = 0 ; i < 105 ; i++){
-		for(int j = 0 ; j < 105 ; j++){
-			a[i][j] = 0;
-		}
-	}
-}
-
-void solve(int n){
-	semt();
-	int x, y;
+void solve(int w , int h , int n){
+	w += 1; h += 1;
+	int a[w+5][h+5] , x , y;
+	memset(a , 0 , sizeof(a));
 	for(int i = 0 ; i < n ; i++){
-		cin >> x >> y;
-		a[x][y] = 1;
-	}
-	int best[w+1][h+1];
+		cin >> x>> y;
+		a[x+1][y+1] = 1;
+	}	
 	
-	for(int i = 0 ; i <= w ; i++){
-		for(int j = 0 ; j <= h ; j++){
-			best[i][j] = INF;
-		}
+	int dp[w+5][h+5];
+        string	ways[w+5][h+5];
+
+	for(int i = 0 ; i <= w+1 ; i++){
+		dp[i][0] = INF;
+	       	ways[i][0] = "0";
+			
 	}
 
-	queue<pair<int , int>> q;
-	string cnt[w+1][h+1];
-	memset(cnt , 0 , sizeof(cnt));
-	cnt[0][0] = "1";
-	best[0][0] = 0;
-	q.push({0 , 0});
-	while(!q.empty()){
-		int x = q.front().first, y = q.front().second;
-		q.pop();
-		//cout << x << " " << y << " cur" << endl; 
-		if(safe(x+1 , y)){
-			//cout << "safe " << x+1 << " " << y << endl;
-			if(best[x+1][y] > best[x][y] + 1){
-				best[x+1][y] = best[x][y] + 1;
-				cnt[x+1][y] = cnt[x][y];
-				q.push({x+1 , y});
-			}else if(best[x+1][y] == best[x][y] + 1)
-				cnt[x+1][y] = cong(cnt[x+1][y] , cnt[x][y]);
-		}
+	for(int i = 0 ; i <= h+1 ; i++){
+		dp[0][i] = INF;
+		ways[0][i] = "0";
+	}
 
-		if(safe(x, y+1)){
-			//cout << "safe " << x << " " << y+1 << endl;
-			if(best[x][y+1] > best[x][y] + 1){
-				best[x][y+1] = best[x][y] + 1;
-				cnt[x][y+1] = cnt[x][y];
-				q.push({x , y+1});
-			}else if(best[x][y] + 1 == best[x][y+1]){
-				cnt[x][y+1] = cong(cnt[x][y+1] ,cnt[x][y]);
+	dp[1][1] = 1;
+	for(int i = 2 ; i <= w+1 ; i++){
+		if(a[i][1] == 1){
+			dp[1][i] = INF;
+			ways[i][1] = "0";
+			continue;
+		}
+		dp[i][1] = dp[i-1][1]+1;
+		ways[i][1] = "1";
+	}
+
+	for(int i = 2 ; i <= h+1 ; i++){
+		if(a[1][i] == 1){
+			dp[1][i] = INF;
+			ways[1][i] = "0";
+			continue;
+		}
+		dp[1][i] = dp[1][i-1] + 1;
+		ways[1][i] = "1";
+	}
+
+	ways[1][1] = "1";
+
+	for(int i = 2 ; i <= w ; i++){
+		for(int j = 2 ; j <= h ; j++){
+			if(a[i][j] == 1){
+				dp[i][j] = INF;
+				ways[i][j] = "0";
+			}else{
+				if(dp[i-1][j] < dp[i][j-1]){
+					dp[i][j] = dp[i-1][j] + 1;
+					ways[i][j] = ways[i-1][j];
+				}else if(dp[i][j-1] < dp[i-1][j]){
+					dp[i][j] = dp[i][j-1] + 1;
+					ways[i][j] = ways[i][j-1];
+				}else{
+					dp[i][j] = dp[i-1][j] + 1;
+					ways[i][j] = cong( ways[i-1][j] , ways[i][j-1] );
+				}
 			}
 		}
 	}
-	cout << cnt[w][h] << endl;
+	
+	cout << "res = " <<ways[w][h] << endl;
 }
 
 int32_t main(){
         ios_base::sync_with_stdio(false);
         cin.tie(0);cout.tie(0);
         FileInput();
-	int n;
-	while(cin >> w >> h >> n)
-        	solve(n);
+	int w , h , n;
+	while(cin >> w>> h){
+        	if(w == 0 && h == 0)
+			break;
+		cin >> n;
+		solve(w , h , n);
+	}
         return 0;
 }        
