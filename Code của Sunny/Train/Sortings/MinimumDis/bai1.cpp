6e0657c570c17b6f03bf3599e6b85169260112e3
SunnyYeahBoiii <vuminhphuongvt12345@gmail.com>
2022-01-22 16:42:52 +0700

    update

--- Code của Sunny/Train/Sortings/MinimumDis/bai1.cpp
+++ Code của Sunny/Train/Sortings/MinimumDis/bai1.cpp
@@ -9,7 +9,7 @@ Code Ideal: I'm too lazy for something like this .__.
 
 using namespace std;
 
-#define NAME "remizdabest"
+#define NAME "bai1"
 #define fast()   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define FileInput() if(NAME != "remizdabest"){freopen(NAME".inp" , "r" , stdin);freopen(NAME".out" , "w" , stdout);}
 #define int long long
@@ -18,8 +18,35 @@ using namespace std;
 
 void solve(){
 
+	int n, m;
+	cin >> n >> m;
 
+	vector<int> a(n) , b(m);
 
+	for(int i = 0 ; i < n ; i++)
+		cin >> a[i];
+
+	for(int i = 0 ; i < m ; i++)
+		cin >> b[i];
+
+	sort(a.begin() , a.end() , greater<int>());
+	sort(b.begin() , b.end());
+
+	int i = 0 , j = m-1;
+
+	int res = abs(a[i] - b[j]);
+
+	while(i < n && j >= 0){
+		int t = a[i] - b[j];
+		if(t < 0)
+			j--;
+		else i++;
+
+		if(abs(t) < res)
+			res = abs(t);
+	}
+
+	cout << res << endl;
 }
 
 int32_t main(){
