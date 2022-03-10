3304b430edc7011973c7e3b0b1f9176d3ae7e4cd
nguyenvn2905 <kiet2905@gmail.com>
2022-03-10 09:11:00 +0700

    update

--- Code của Nguyên/README.md
+++ Code của Nguyên/README.md
@@ -4,4 +4,6 @@
 
 - Hôm nay là ngày thứ 3 tôi bỏ game, tôi đã bắt đầu biết bay, thính giác của tôi cảm thấy có thể nghe được tiếng rên Sunny một cách thích thú
 
-
+## 10/3 :
+- Hôm nay là ngày thứ 4 tôi bỏ game, việc thấy được bà hàng xóm tắm là quá bth vs tôi, các giác quan của tôi linh hoạt 1 cách lạ thường
+- Đã cài lại óu! :(
---
+++ Code của Nguyên/êvday codẻ/rbpoint2/geometry/a.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define FOR(x,a,b) for (int x=a;x<=b;x++)
+#define FOD(x,a,b) for (int x=a;x>=b;x--)
+#define pub push_back
+#define pob pop_back
+#define ii  pair<int,int>
+#define pll pair<long long, long long>
+#define F first
+#define S second
+typedef unsigned long long int ull;
+typedef long long ll;
+const long N=1e5 + 5;
+using namespace std;
+void solve(){
+    long n,m,k;
+    cin>>n >> m >> k;
+    long x = ceil (sqrt(k));
+    FOR(i,1,x)
+        if ( k/i + m == i + n){
+            cout << i + n;
+            return;
+        }
+         else if ( k/i + n == i + m) {
+            cout << i + m;
+            return ;
+        }
+    cout << -1;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen(".inp","r",stdin);
+    // freopen(".out","w",stdout);
+    solve();  
+    return 0;
+}
