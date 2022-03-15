913481da88ceceb3063c03dfd20654770693f257
nguyenvn2905 <kiet2905@gmail.com>
2022-03-15 18:19:48 +0700

    update

--- Code của Nguyên/êvday codẻ/15.3/hinhvuong/hinhvuong.cpp
+++ Code của Nguyên/êvday codẻ/15.3/hinhvuong/hinhvuong.cpp
@@ -14,7 +14,25 @@ const long N=1e5 + 5;
 const int mod = 1e9 +7;
 using namespace std;
 void solve(){
+    int m,n;
+    int a[1000][1000],b[1000][1000];int res;
+    cin >> m >> n;
+    FOR(i,0,m-1)
+        FOR(j,0,n-1) cin >> a[i][j];
 
+    FOR(i,0,m-1) b[0][i]=a[0][i];
+
+    FOR(i,0,n-1) b[i][0]=a[i][0];
+
+    FOR(i,1,n-1)
+        FOR(j,1,m-1)
+            b[i][j]=min(min(b[i-1][j-1],b[i][j-1]),b[i-1][i])+1;
+
+    res=b[0][0];
+    FOR(i,0,m-1)
+        FOR(j,0,n-1)
+            if (res < b[i][j]) res=b[i][j];
+    cout << res*res;
 }
 int main(){
     fast;
