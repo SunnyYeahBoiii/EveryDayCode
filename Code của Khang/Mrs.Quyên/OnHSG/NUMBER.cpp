96434bae006c0125e74e90e658320c029afcc236
Thái Khang <82371312+thkhang06@users.noreply.github.com>
2022-03-28 20:44:32 +0700

    Update NUMBER.cpp

--- Code của Khang/Mrs.Quyên/OnHSG/NUMBER.cpp
+++ Code của Khang/Mrs.Quyên/OnHSG/NUMBER.cpp
@@ -15,19 +15,22 @@ int a[25],x[25],n,d1 = 0,cnt = 0;
 void thu1(int k) {
     FOR(i,0,1) {
         x[k] = i;
-        if (x[k] == 1) d1++;
-        if (k == n && d1 >= 2) {
+        if (k == n) {
             bool ok;
-            FOR(j,1,n) if (x[j] == 1 && a[j]&1) {ok = false;break;}
-            else if (x[j] == 1 && a[j]%2 == 0) {ok = true;break;}
-            cnt++;
-            FOR(j,1,n) {
-                if (ok && x[j] == 1 && a[j]%2 == 0) ok = false;
-                else if (!ok && x[j] == 1 && a[j]%2 != 0) ok = true;
-                else if (ok && x[j] == 1 && a[j]%2 != 0) {cnt--;break;}
-                else if (!ok && x[j] == 1 && a[j]%2 == 0) {cnt--;break;}
-            }
             d1 = 0;
+            FOR(j,1,n) if (x[j] == 1) d1++;
+            if (d1 >= 2) {
+                FOR(j,1,n) if (x[j] == 1 && a[j]&1) {ok = false;break;}
+                else if (x[j] == 1 && a[j]%2 == 0) {ok = true;break;}
+                cnt++;
+                FOR(j,1,n) {
+                    if (ok && x[j] == 1 && a[j]%2 == 0) ok = false;
+                    else if (!ok && x[j] == 1 && a[j]%2 != 0) ok = true;
+                    else if (ok && x[j] == 1 && a[j]%2 != 0) {cnt--;break;}
+                    else if (!ok && x[j] == 1 && a[j]%2 == 0) {cnt--;break;}
+                }
+                d1 = 0;
+            }
         } else thu1(k+1);
     }
 }
