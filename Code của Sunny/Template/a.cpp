d89c2b7e8f065b1f305e9226ad6093332634355b
Sunny <91402650+SunnyYeahBoiii@users.noreply.github.com>
2022-04-07 22:08:56 +0700

    update

--- Code của Sunny/Template/a.cpp
+++ Code của Sunny/Template/a.cpp
@@ -1,34 +1,100 @@
-// Link : 
-// Author : MinhPhuongVu A.K.A SunnyYeahBoii
-
-/* 
-Code Ideal: I'm too lazy for something like this .__.
+/*
+SunnyYeahBoi aka MinhPhuongVu
+It's My Time To Shine Bois
 */
-
 #include "bits/stdc++.h"
 
 using namespace std;
 
-#define NAME "remizdabest"
-#define fast()   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define FileInput() if(NAME != "remizdabest"){freopen(NAME".inp" , "r" , stdin);freopen(NAME".out" , "w" , stdout);}
-#define int long long
+#define ll long long
+#define int ll
 #define endl "\n"
-#define INF LLONG_MAX
 
+const int INF = LLONG_MAX;
+const int LOW = LLONG_MIN;
+const int MOD = 07032006;
+
+// BITWISE-------------------------------------
+
+template <typename x>
+    inline void ONBIT(x & a , x b){
+        a = a | (1 << b);
+    }
+
+template <typename x>
+    inline void REVBIT(x & a , x b){
+        a = a ^ (1 << b);
+    }
+
+template<typename x>
+    inline bool ISON(x & a , x b){
+        return ((a >> b) & 1);
+    }
+
+//---------------------------------------------
+
+// NUMERIC-------------------------------------
+
+template <typename x>
+    inline x gcd (x a , x b){
+        x r;
+        while(b > 0){
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+
+template <typename x>
+    inline x bcnn (x a , x b){
+        return a / gcd(a , b) * b;
+    }
+
+template <typename x>
+    inline x pow_mod(x a , x b){
+        if(b <= 0)
+            return 1;
+        if(b == 1)
+            return a;
+
+        int t = pow_mod(a , b / 2);
+        if(b % 2 == 0)
+            return (t * t) % MOD;
+        else return ((t * t) % MOD * a) % MOD;
+    }
+
+// REMEMBER A ^ P-2 = A ^ -1 (MOD P)
+
+//---------------------------------------------
+
+void FileInput(string s){
+    if(s == "")
+        return;
+    freopen((s + ".INP").c_str() , "r" , stdin);
+    freopen((s + ".OUT").c_str() , "w" , stdout);
+}
+
+void fastIO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+}
 
 void solve(){
 
 }
 
-int32_t main(){
-	FileInput();
-	fast();     
-	/*
-	int t;
+void multi(){
+    int t;
     cin >> t;
     while(t--)
-	*/
+        solve();
+}
+
+signed main(){
+	FileInput("");
+	fastIO();
+	//multi();
 	solve();
-	return 0;	
+	return 0;
 }
