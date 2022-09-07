bed90d079da1844acaafd9403b01873f27bdeae3
Thái Khang <82371312+thkhang06@users.noreply.github.com>
2022-09-07 11:10:50 +0700

    Update POINTL4.cpp

--- Code của Khang/Mr.Huy/11Tin01/POINTL4.cpp
+++ Code của Khang/Mr.Huy/11Tin01/POINTL4.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 typedef long long ll;
+#define FOR(i,l,r) for (int i=l;i<=r;i++)
+#define FOD(i,r,l) for (int i=r;i>=l;i--)
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pub push_back
+#define pob pop_back
+#define ii pair<int,int>
+#define pll pair<long long, long long>
+#define F first
+#define S second
+typedef unsigned long long int ull;
+using namespace std;
 void solve(){
     ll x0, y0, x1, y1, x2, y2;
     cin >> x0 >> y0 >> x1 >> y1 >> x2 >> y2;
@@ -9,7 +19,7 @@ void solve(){
         a = y1-y2;
         b = x2-x1;
         c = x1*y2-x2*y1;
-        if (a*x0+b&y0+c == 0 && (x0-x1)(x2-x1) >= 0 && (y0-y1)(y2-y1) >= 0) cout << 1 << '\n';
+        if (a*x0+b*y0+c == 0 && (x0-x1)*(x2-x1) >= 0 && (y0-y1)*(y2-y1) >= 0) cout << 1 << '\n';
         else cout << 0 << '\n';
     //}
 }
