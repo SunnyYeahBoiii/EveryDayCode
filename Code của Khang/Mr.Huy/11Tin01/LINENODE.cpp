0f5436738272654bea9ed91f0da092f9f341c9d0
Thái Khang <82371312+thkhang06@users.noreply.github.com>
2022-09-07 11:10:36 +0700

    Update LINENODE.cpp

--- Code của Khang/Mr.Huy/11Tin01/LINENODE.cpp
+++ Code của Khang/Mr.Huy/11Tin01/LINENODE.cpp
@@ -1,19 +1,36 @@
 #include <bits/stdc++.h>
 typedef long long ll;
+#define FOR(i,l,r) for (int i=l;i<=r;i++)
+#define FOD(i,r,l) for (int i=r;i>=l;i--)
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-long double x,y;
+#define pub push_back
+#define pob pop_back
+#define ii pair<int,int>
+#define pll pair<long long, long long>
+#define F first
+#define S second
+typedef unsigned long long int ull;
+using namespace std;
 void solve(){
-    ll  D,Dx,Dy,a1, b1, c1, a2, b2, c2;
+    double d,d1,d2,x,y;
+    ll a1, b1, c1, a2, b2, c2;
     cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
     //while (cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2) {
-        D = a1*b2-a2*b1;
-        Dx = b1*c2 –b2*c1;
-        Dy = c1*a2-c2*a1;
-        x = Dx/D;
-        y = Dy/D;
-        if (D != 0) cout << 1 << setprecision(2) << fixed << x << ' ' << y;
-        else if (D == 0 && (Dx != 0 || Dy != 0)) cout << "0\n";
-        else if (D == 0 && Dx == Dy && Dx == D) cout << "2\n";
+        d = a1*b2-a2*b1;
+        d1 = b1*c2-b2*c1;
+        d2 = c1*a2-c2*a1;
+        x = d1/d;
+        y = d2/d;
+        if (d1 == 0) x = abs(x);
+        if (d2 == 0) y = abs(y);
+        if (d != 0) {
+            cout << 1 << ' ';
+            //cout << d1 << ' ' << d2 << ' ' << d << '\n';
+            cout << setprecision(2) << fixed << x << ' ';
+            cout << setprecision(2) << fixed << y << '\n';
+        }
+        else if (d == 0 && (d1 != 0 || d2 != 0)) cout << 0 << '\n';
+        else if (d == 0 && d1 == d2 && d1 == d) cout << 2 << '\n';
     //}
 }
 int main(){
