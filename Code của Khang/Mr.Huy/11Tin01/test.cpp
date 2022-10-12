5b858810d78b5fe149296eed658550b4dc53f841
Thái Khang <82371312+thkhang06@users.noreply.github.com>
2022-10-12 21:58:11 +0700

    Bill.cpp

--- Code của Khang/Mr.Huy/11Tin01/test.cpp
+++ Code của Khang/Mr.Huy/11Tin01/test.cpp
@@ -11,22 +11,32 @@ typedef long long ll;
 #define S second
 typedef unsigned long long int ull;
 using namespace std;
+double ro(double var)
+{
+    double value = (int)(var * 10000 + .5);
+    return (double)value / 10000;
+}
 void solve(){
     int n;
     cin >> n;
     ll x[n],y[n];
-    double hs = 0,kmin = INT_MAX,kq = 0;
+    double p,c,hs = 0,kmin = INT_MAX,kq[n];
     cin >> x[0] >> y[0];
     FOR(i,1,n) cin >> x[i] >> y[i];
     FOR(i,1,n) {
-        kq = sqrt((x[i]-x[0])*(x[i]-x[0])+(y[i]-y[0])*(y[i]-y[0]));
-        kmin = min(kmin,kq);
+        kq[i] = sqrt((x[i]-x[0])*(x[i]-x[0])+(y[i]-y[0])*(y[i]-y[0]));
+        kmin = min(kmin,kq[i]);
         //cout << kmin << ' ' << kq << '\n';
     }
     FOR(i,2,n) {
-        hs = abs((-y[i]+y[i-1])*x[0]+(x[i]-x[i-1])*y[0]+(-y[i]+y[i-1])*(-x[0])+(x[i]-x[i-1])*(-y[0]))/sqrt((-y[i]+y[i-1])*(-y[i]+y[i-1])+(x[i]-x[i-1])*(x[i]-x[i-1]));
+        c = ro(sqrt((x[i]-x[i-1])*(x[i]-x[i-1])+(y[i]-y[i-1])*(y[i]-y[i-1])));
+        kq[i] = ro(kq[i]); kq[i-1] = ro(kq[i-1]);
+        if ((kq[i]*kq[i] < (kq[i-1]*kq[i-1]+c*c)) && (c*c < (kq[i-1]*kq[i-1]+kq[i]*kq[i])) && (kq[i-1]*kq[i-1] < (kq[i]*kq[i]+c*c))) {
+            p = (kq[i]+kq[i-1]+c)/2;
+            hs = 2*sqrt(p*(p-kq[i])*(p-kq[i-1])*(p-c))/c;
+        }
         kmin = min(kmin,hs);
-        cout << kmin << ' ' << hs << '\n';
+        //cout << kmin << ' ' << hs << '\n';
     }
     cout << setprecision(4) << fixed << kmin;
 }
