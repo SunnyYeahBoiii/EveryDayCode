86a1bfa4501ed4c7e63253083bf6ae80e1e6e262
Thái Khang <82371312+thkhang06@users.noreply.github.com>
2023-02-13 16:39:59 +0700

    Update TREES.cpp

--- Code của Khang/Mrs.Quyên/OnHSG/TREES.cpp
+++ Code của Khang/Mrs.Quyên/OnHSG/TREES.cpp
@@ -11,23 +11,45 @@ typedef long long ll;
 #define S second
 typedef unsigned long long int ull;
 using namespace std;
+int n,k;
+ll b[10005];
+int bs(vector<ll> &a,int l,int r,int k){
+    while (r-l > 1) {
+        int m = l+(r-l)/2;
+        if (a[m] >= k) r = m;
+        else l = m;
+    }
+    return r;
+}
 void solve(){
-    int n,k;
     cin >> n >> k;
-    int a[n],d = 1,d1 = 1;
-    FOR(i,0,n-1) cin >> a[i];
-    FOR(i,1,k-2) {
-        if (a[i] > a[i-1]) ++d;
-        else d = 1;
+    vector<ll> a;
+    FOR(i,0,n-1) cin >> b[i];
+    int d = 0,l = n;
+    FOR(i,0,k-2) {
+        if (b[i] < b[k-1]) a.pub(b[i]),d++;
     }
+    //a.pub(b[k-1]);
     FOR(i,k-1,n-1) {
-        if (a[i] > a[i-1]) ++d1;
-        else d1 = 1;
+        if (b[i] >= b[k-1]) a.pub(b[i]),d++;
     }
-    cout << (k-1-d)+(n-k-d1);
+    n = d;
+    //FOR(i,0,n-1) cout << a[i] << " ";
+    vector<ll> dp(n,0);
+    dp[0] = a[0];
+    int res = 1;
+    FOR(i,1,n-1) {
+        if (a[i] < dp[0]) dp[0] = a[i];
+        else if (a[i] > dp[res-1]) dp[res++] = a[i];
+        else dp[bs(dp,-1,res-1,a[i])] = a[i];
+    }
+    //FOR(i,0,res-1) cout << dp[i] << " ";
+    cout << l-res;
 }
 int main(){
     fast;
+    // freopen("TREES.inp","r",stdin);
+    // freopen("TREES.out","w",stdout);
     solve();
     return 0;
 }
